626506d717a7ae0a9ba9fd438c99349dd7b27934
Zsolt Fehér <feherzsolt999@gmail.com>
2025-11-21 07:36:16 +0100

    Add AutoComplete expectation to mock server + README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+# Docker build
+`docker build -t spring-app`
+
+# Docker run
+`docker run -it -p 8080:8080 spring-app`
--- src/main/java/hu/smith/software/mockserver/demo/expectations/ExpectationsConfiguration.java
+++ src/main/java/hu/smith/software/mockserver/demo/expectations/ExpectationsConfiguration.java
@@ -3,6 +3,7 @@ package hu.smith.software.mockserver.demo.expectations;
 import org.mockserver.client.MockServerClient;
 import org.mockserver.model.HttpRequest;
 import org.mockserver.model.HttpResponse;
+import org.mockserver.model.JsonBody;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -14,19 +15,24 @@ public class ExpectationsConfiguration implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        new MockServerClient(mockserverHost, 1080)
-            .when(
-                HttpRequest.request()
-                    .withMethod("GET")
-                    .withPath("/java")
-            )
-            .respond(
-                HttpResponse.response()
-                    .withStatusCode(200)
-                    .withHeader("Access-Control-Allow-Origin", "*")
-                    .withBody("Hello from Java")
-            );
+        MockServerClient client = new MockServerClient(mockserverHost, 1080);
+        client.when(
+            HttpRequest.request().withMethod("GET").withPath("/java")
+        ).respond(
+            HttpResponse.response().withStatusCode(200).withHeader(
+                "Access-Control-Allow-Origin", "*"
+            ).withBody("Hello from Java")
+        );
 
         System.out.println("Java expectation set for /java");
+
+        client.when(
+            HttpRequest.request().withMethod("GET").withPath("/autoComplete")
+        ).respond(
+            HttpResponse.response().withStatusCode(200).withHeader(
+                "Access-Control-Allow-Origin", "*"
+            ).withBody(JsonBody.json("{\"message\": [\"alma\", \"körte\", \"citrom\"]}"))
+        );
+        System.out.println("Java expectation set for /autoComplete");
     }
 }
